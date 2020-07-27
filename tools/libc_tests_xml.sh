d5197946ddc29cc362deef1f53009464e6a528df
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2020-07-26 18:10:27 -0700

    Changed to a generic runner script.

--- tools/libc_tests_xml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Running libc tests."
-export CMOCKA_XML_FILE="$MESON_BUILD_ROOT/test/%g.xml"
-echo "CMOCKA_XML_FILE=$CMOCKA_XML_FILE"
-$MESON_BUILD_ROOT/test/libc_test
-
---
+++ tools/run_with_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export $2
+eval $1
+
